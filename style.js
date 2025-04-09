dea48db82ed30a74f0d0d768a91f2c357bbbc57e
cibelly563 <cibellyandrielly34@gmail.com>
2025-04-09 01:50:30 +0000

    novas alterações!

---
+++ style.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    // Alerta de boas-vindas
+    alert("Bem-vindo ao nosso portfólio!");
+
+    // Abrir redes sociais em nova aba
+    const botoesRedes = document.querySelectorAll("button[data-link]");
+    botoesRedes.forEach(botao => {
+        botao.addEventListener("click", () => {
+            const link = botao.getAttribute("data-link");
+            window.open(link, "_blank");
+        });
+    });
